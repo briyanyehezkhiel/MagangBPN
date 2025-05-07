2f91599d45d28a7070f6da3b79610b31e4a980be
briyanyehezkhiel <briyanyehezkhiel@gmail.com>
2025-05-07 22:12:38 +0700

    buat yang akses hanya dapat admin

--- app/Filament/Resources/PNResource/Pages/EditPN.php
+++ app/Filament/Resources/PNResource/Pages/EditPN.php
@@ -5,11 +5,20 @@ namespace App\Filament\Resources\PNResource\Pages;
 use App\Filament\Resources\PNResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Auth;
 
 class EditPN extends EditRecord
 {
     protected static string $resource = PNResource::class;
 
+    public function mount(string|int $record): void
+    {
+        parent::mount($record);
+
+            if (Auth::check() && Auth::user()->role !== 'admin') {
+                $this->redirect('admin/pns'); // tanpa return
+            }
+        }
     protected function getHeaderActions(): array
     {
         return [
--- app/Filament/Resources/PTUNResource/Pages/EditPTUN.php
+++ app/Filament/Resources/PTUNResource/Pages/EditPTUN.php
@@ -5,11 +5,20 @@ namespace App\Filament\Resources\PTUNResource\Pages;
 use App\Filament\Resources\PTUNResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Auth;
 
 class EditPTUN extends EditRecord
 {
     protected static string $resource = PTUNResource::class;
 
+    public function mount(string|int $record): void
+    {
+        parent::mount($record);
+
+            if (Auth::check() && Auth::user()->role !== 'admin') {
+                $this->redirect('admin/ptuns'); // tanpa return
+            }
+        }
     protected function getHeaderActions(): array
     {
         return [
