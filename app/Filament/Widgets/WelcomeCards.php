bc6c39b69da3ccc5f045834be50b61ebf6fb0213
briyanyehezkhiel <briyanyehezkhiel@gmail.com>
2025-05-07 22:12:59 +0700

    buat dia di posisi 1 chart

--- app/Filament/Widgets/WelcomeCards.php
+++ app/Filament/Widgets/WelcomeCards.php
@@ -6,5 +6,7 @@ use Filament\Widgets\Widget;
 
 class WelcomeCards extends Widget
 {
+    protected static ?int $sort = 1;
+    // protected int | string | array $columnSpan = 'full';
     protected static string $view = 'filament.widgets.welcome-cards';
 }
